c5b118a89334677b7ef211074add6950a5a6f838
Justin Dyson <jdyson@fullsail.edu>
2014-01-19 10:07:56 -0600

    showStats function added

--- JSONdata.js
+++ JSONdata.js
@@ -2,27 +2,40 @@
 
 //alert("JavaScript works!");
 
-var json = {
-	"pokemon": [
+var pokedex = {
+	"myPokemon": [
 		{
 			"name": "Pickachu",
-			"hp": 50,
-			"attackList": ["Thundershock", "Leer", "Thunderwave"]
+			"hp": 75,
+			"attackList": ["Thundershock", "Leer", "Thunderwave"],
+			"attackPoints": [30, 0, 0],
+			"attackDescription": ["Does 30 damage", "Lowers enemy defense by 1", "Paralyzes the enemy"],
+			"onBelt": true
 		},
 		{
 			"name": "Charmander",
-			"hp": 50,
-			"attackList": ["Ember", "Scratch", "Growl"]
+			"hp": 70,
+			"attackList": ["Ember", "Scratch", "Growl"],
+			"attackPoints": [30, 25, 0],
+			"attackDescription": ["Does 30 damage", "Does 25 damage", "Lowers enemy's attack by 1"],
+			"onBelt": true
 		},
 		{
 			"name": "Squirtle",
-			"hp": 50,
-			"attackList": ["Water Gun", "Leer", "Bubble"]	
-		},
+			"hp": 80,
+			"attackList": ["Water Gun", "Leer", "Bubble"],
+			"attackPoints": [30, 0, 35],
+			"attackDescription": ["Does 30 damage", "Lowers enemy defense by 1", "Does 35 Damage"],
+			"onBelt": true
+		}
+	],
+	"wildPokemon": [
 		{
 			"name": "Bulbasaur",
-			"hp": 40,
-			"attackList": ["Tackle", "Growl"]
+			"hp": 65,
+			"attackList": ["Tackle", "Growl"],
+			"attackPoints": [25, 0,],
+			"onBelt": false
 		}
 	]
 };
--- dyson_justin_assignment3.js
+++ dyson_justin_assignment3.js
@@ -5,11 +5,70 @@ Term 1401
 Completed: 01/23/14
 */
 
-var handleData = function (json) {
-	for (i = 0; i < json.pokemon.length; i++) {
-		var pokemon = json.pokemon[i];
-		console.log("Name: " + pokemon.name + ", hit points: " + pokemon.hp + ", list of attacks: " + pokemon.attackList); 	
+
+
+// Global Variables
+var numberOfMyPokemon = pokedex.myPokemon.length;	// number of pokemon that are on PokeBelt
+var chosenPokemon = "";	// holds string for pokemon chosen by user
+var pokemonName; // returned string for showStats function
+var changePokemon = true; // boolean for confirm() after showStats function
+
+// Functions
+
+var showStats = function (pokemon) {		// shows stats for pokemon chosen by user, string is passed in
+	for (i = 0; i < pokedex.myPokemon.length; i++) {
+		if (pokemon === pokedex.myPokemon[i].name) {
+			console.log("---");
+			console.log("Pokemon Name: " + pokedex.myPokemon[i].name);
+			console.log("Total HP: " + pokedex.myPokemon[i].hp);
+			console.log("Attacks:");
+			console.log(pokedex.myPokemon[i].attackList[0] + ": " + pokedex.myPokemon[i].attackDescription[0] );
+			console.log(pokedex.myPokemon[i].attackList[1] + ": " + pokedex.myPokemon[i].attackDescription[1] );
+			console.log(pokedex.myPokemon[i].attackList[2] + ": " + pokedex.myPokemon[i].attackDescription[2] );
+		};
 	};
+
+	return pokemon; // returns string of pokemonName
 };
 
-handleData(json);
+
+// Main Code
+
+console.log("Wild Bulbasaur attacks!");
+
+console.log("You have " + numberOfMyPokemon + " Pokemon on your PokeBelt!" );
+
+chosenPokemon = prompt("Choose one of your Pokemon to view their stats.", "Charmander, Pickachu, Squirtle");
+
+pokemonName = showStats(chosenPokemon);		// displays stats for pokemon(string) passed in from user prompt
+
+changePokemon = confirm("Do you want to use " + pokemonName + "?");
+
+
+
+
+
+//console.log(numberOfMyPokemon);
+
+
+
+
+
+
+
+// EXAMPLE CODE
+/*
+for (i = 0; i < json.pokemon.length; i++) {
+	var myPokemon = [];
+	 
+	while (json.pokemon[i].onBelt) {
+		myPokemon.push(json.pokemon[i].name);
+		i++;
+	};
+	
+	
+	console.log("My Pokemon are " + myPokemon);
+	
+};
+
+*/
