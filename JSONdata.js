b3dd6fbe0da54aca19969df70e03d20e1b16389f
Justin Dyson <jdyson@fullsail.edu>
2014-01-18 22:22:20 -0600

    json data created

---
+++ JSONdata.js
@@ -0,0 +1,28 @@
+// JSON data
+
+//alert("JavaScript works!");
+
+var json = {
+	"pokemon": [
+		{
+			"name": "Pickachu",
+			"hp": 50,
+			"attackList": ["Thundershock", "Leer", "Thunderwave"]
+		},
+		{
+			"name": "Charmander",
+			"hp": 50,
+			"attackList": ["Ember", "Scratch", "Growl"]
+		},
+		{
+			"name": "Squirtle",
+			"hp": 50,
+			"attackList": ["Water Gun", "Leer", "Bubble"]	
+		},
+		{
+			"name": "Bulbasaur",
+			"hp": 40,
+			"attackList": ["Tackle", "Growl"]
+		}
+	]
+};
--- dyson_justin_assignment3.js
+++ dyson_justin_assignment3.js
@@ -1,1 +1,15 @@
-alert("JavaScript works!");
+/*
+By Justin Dyson
+SDI Assignment 3
+Term 1401
+Completed: 01/23/14
+*/
+
+var handleData = function (json) {
+	for (i = 0; i < json.pokemon.length; i++) {
+		var pokemon = json.pokemon[i];
+		console.log("Name: " + pokemon.name + ", hit points: " + pokemon.hp + ", list of attacks: " + pokemon.attackList); 	
+	};
+};
+
+handleData(json);
